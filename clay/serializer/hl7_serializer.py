27cfd67181661debcfd3ea73529d6dc8b39f0ffe
Vittorio Meloni <vittorio.meloni@crs4.it>
2015-02-18 20:48:33 +0100

    Fixed raising of InvalidMessage

--- clay/serializer/hl7_serializer.py
+++ clay/serializer/hl7_serializer.py
@@ -18,7 +18,7 @@ class AbstractHL7Serializer(Serializer):
         try:
             self.serializer = self.SERIALIZERS[message_type]()
         except KeyError:
-            raise InvalidMessage
+            raise InvalidMessage(message_type)
 
     def serialize(self, datum):
         return self.serializer.serialize(datum)
@@ -30,7 +30,7 @@ class AbstractHL7Serializer(Serializer):
         try:
             serializer = cls.SERIALIZERS[msg_type]
         except KeyError:
-            raise InvalidMessage
+            raise InvalidMessage(msg_type)
         return serializer.deserialize(message, catalog)
 
 # vim:tabstop=4:expandtab
