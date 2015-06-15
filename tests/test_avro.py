52341bd6da4f13a4469a8106eff262c1b93283eb
Vittorio Meloni <vittorio.meloni@crs4.it>
2015-06-15 14:53:37 +0200

    Added pyavroc tests

--- tests/test_avro.py
+++ tests/test_avro.py
@@ -24,6 +24,7 @@ from unittest import TestCase
 from clay.exceptions import SchemaException
 from clay.factory import MessageFactory
 from clay.serializer.avro_serializer import AvroSerializer
+from clay.serializer.pyavroc_serializer import AvroSerializer as PyAvrocSerializer
 
 from tests import TEST_CATALOG
 
@@ -31,6 +32,8 @@ from tests import TEST_CATALOG
 class TestAvroSerializer(TestCase):
     def setUp(self):
         self.avro_factory = MessageFactory(AvroSerializer, TEST_CATALOG)
+        self.pyavroc_factory = MessageFactory(PyAvrocSerializer, TEST_CATALOG)
+        self.factories = (self.avro_factory, self.pyavroc_factory)
 
         self.simple_msg_content = {"id": 1111111, "name": u"aaa"}
         self.complex_msg_content = {
@@ -51,38 +54,48 @@ class TestAvroSerializer(TestCase):
         }
 
         self.avro_simple = self.avro_factory.create("TEST")
+        self.pyavroc_simple = self.pyavroc_factory.create("TEST")
+
         self.avro_simple.set_content(self.simple_msg_content)
+        self.pyavroc_simple.set_content(self.simple_msg_content)
         self.simple_encoded = "\x00\x10\x8e\xd1\x87\x01\x06aaa"
 
         self.avro_complex = self.avro_factory.create("TEST_COMPLEX")
+        self.pyavroc_complex = self.pyavroc_factory.create("TEST_COMPLEX")
+
         self.avro_complex.set_content(self.complex_msg_content)
+        self.pyavroc_complex.set_content(self.complex_msg_content)
         self.complex_encoded = '\x02l\x01\x8e\xd1\x87\x01\x80\x80\xa0\xf6\xf4\xac\xdb\xe0\x1b-\xb2\x9d?&\xa6' \
                                '\xac\xaa\x04\xb6y3\x06aaa\x00\x02\x06bbb\x00\x00\x02\x06ccc\x00\x00\x06ddd\x00\x06eee'
 
     def test_retrieve(self):
-        m = self.avro_factory.retrieve(self.complex_encoded)
+        for factory in self.factories:
+            m = factory.retrieve(self.complex_encoded)
 
-        self.assertEqual(m.valid, True)
-        self.assertEqual(m.id, 1111111)
-        self.assertEqual(m.long_id, 10**18)
-        self.assertEqual(round(m.float_id, 3), 1.232)  # TODO: Avro seems to have wrong behavior with float
-        self.assertEqual(m.double_id, 1e-60)
-        self.assertEqual(m.name, "aaa")
-        self.assertEqual(len(m.array_complex_field), 1)
-        self.assertEqual(m.array_complex_field[0].field_1, "bbb")
-        self.assertEqual(len(m.array_simple_field), 1)
-        self.assertEqual(m.array_simple_field[0], "ccc")
-        self.assertEqual(m.record_field.field_1, "ddd")
-        self.assertEqual(m.record_field.field_2, "eee")
+            self.assertEqual(m.valid, True)
+            self.assertEqual(m.id, 1111111)
+            self.assertEqual(m.long_id, 10**18)
+            self.assertEqual(round(m.float_id, 3), 1.232)  # TODO: Avro seems to have wrong behavior with float
+            self.assertEqual(m.double_id, 1e-60)
+            self.assertEqual(m.name, "aaa")
+            self.assertEqual(len(m.array_complex_field), 1)
+            self.assertEqual(m.array_complex_field[0].field_1, "bbb")
+            self.assertEqual(len(m.array_simple_field), 1)
+            self.assertEqual(m.array_simple_field[0], "ccc")
+            self.assertEqual(m.record_field.field_1, "ddd")
+            self.assertEqual(m.record_field.field_2, "eee")
 
     def test_avro_serializer(self):
-        value = self.avro_simple.serialize()
-        self.assertEqual(value, self.simple_encoded)
+        for m in (self.avro_simple, self.pyavroc_simple):
+            value = m.serialize()
+            self.assertEqual(value, self.simple_encoded)
 
-        value = self.avro_complex.serialize()
-        self.assertEqual(value, self.complex_encoded)
+        for m in (self.avro_complex, self.pyavroc_complex):
+            value = m.serialize()
+            self.assertEqual(value, self.complex_encoded)
 
     def test_wrong_schema(self):
-        # setting wrong type for the value (it should be int)
-        self.avro_complex.id = "111111"
-        self.assertRaises(SchemaException, self.avro_complex.serialize)
+        for m in (self.avro_complex, self.pyavroc_complex):
+            # setting wrong type for the value (it should be int)
+            m.id = "111111"
+            self.assertRaises(SchemaException, m.serialize)
